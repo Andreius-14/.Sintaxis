a638d70b6ca8d00263191f113031bc8fe5083f03
Andreius_14 <Andreius_14@outlook.com>
2023-10-22 18:07:21 -0500

    3

--- 1.hola_mundo.sh
+++ 1.hola_mundo.sh
@@ -2,9 +2,16 @@
 # Do: Imprime un hola mundo
 # Autor: Andreius_14
 
+nombre_carlos="Carlos Armando Andres"
+numero_1=1
+numero_2=100
+numero_3=5
+numero_4=7
+numero_5=8
+
 echo 'Hola mundo sh !!!'
 echo $NOMBRE
-
+echo $nombre_carlos
 exit 0
 # exit 1 - Errores generales, ejemplo:Division por cero
 # exit 2 - Errores de Shell
