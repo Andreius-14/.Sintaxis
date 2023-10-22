f21509460669235180ba8fc758770040fa50ab9a
Andreius_14 <Andreius_14@outlook.com>
2023-10-22 18:28:04 -0500

    Operadores logicos y Aritmeticos.

---
+++ 2.Operando_Variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Do: Imprime un hola mundo
+# Autor: Andreius_14
+
+parametro_1=40
+parametro_2=10
+
+echo "==========Operadores Aritmeticos=========="
+
+echo $((parametro_1+parametro_2))
+echo $((parametro_1-parametro_2))
+echo $((parametro_1*parametro_2))
+echo $((parametro_2/parametro_2))
+
+echo "==========Operaciones Logicas=========="
+
+echo $((parametro_1>parametro_2))   #True 1
+echo $((parametro_1<parametro_2))   #false 0
+
+
+
