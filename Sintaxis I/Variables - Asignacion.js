20bca2ecefd38914b0c21ba8b74357d8f4de99e4
Andreius_14 <Andreius_14@outlook.com>
2026-06-24 18:30:31 -0500

    (add) variables

--- Sintaxis I/Variables - Asignacion.js
+++ Sintaxis I/Variables - Asignacion.js
@@ -1,7 +1,10 @@
 // ┌───────────────────────────────────┐
 // │                &&                 │
 // └───────────────────────────────────┘
-
+// Mis Notas Personales:
+// - No devuelve un lado como solo arroja true o false
+// - Como en matematiccas tiene prioridad sobre ||
+// - Como matematicas eliges cual tiene prioridad con ()
 const nombre1 = "Ana";
 const edad1 = 25;
 
@@ -19,10 +22,31 @@ console.log(loginValido); // Imprime: "" (devuelve el primer valor falsy: usuari
 const mensaje = nombre && "Bienvenida, " + nombre;
 console.log(mensaje); // Imprime: "Bienvenida, Ana"
 
+// - Si existe lo reemplazo
+// - Reemplazo SI, La Derecha es True
+const accion = 1;
+const b = 0;
+
+accion &&= 2;
+console.log(accion); // Expected output: 2
+
+b &&= 2;
+console.log(b); // Expected output: 0
 // ┌───────────────────────────────────┐
 // │                ||                 │
 // └───────────────────────────────────┘
-
+// [Operador lógico OR]
+// - Omite  false
+// - Omite  ""
+// - Omite  ''
+// - Omite  ``
+// - Omite  0
+// - Omite  undefined
+// - Omite  NaN
+//
+// Mis Notas Personales:
+// - Devuelve el lado que lance True
+//
 let nombre = process.env.NOMBRE || "Sin nombre";
 let web = process.env.WEB || "no tengo web";
 
@@ -50,6 +74,13 @@ console.log(configFinal); // Imprime: { tema: "oscuro" }
 // ┌───────────────────────────────────┐
 // │                ??                 │
 // └───────────────────────────────────┘
+// [operador de fusión nulo]
+// - Detecta Null
+// - Detecta undefined
+//
+// Mis Notas Personales:
+// - Es como el respaldo de Emergencia
+// - Una mejor alternativa a || ya que este erra con el numero "0" o "" cadenas vacias
 function saluda(nombre) {
   nombre ??= "Alumno"; // Si no tiene valor (undefined), valdrá "Alumno"
   alert("Hola " + nombre);
@@ -60,7 +91,13 @@ saluda("Pepe"); // Hola Pepe
 // ┌───────────────────────────────────┐
 // │                 ?                 │
 // └───────────────────────────────────┘
-
+// [Operador de encadenamiento opcional]
+// - Detecta Null
+// - Detecta undefined
+//
+// Mis Notas Personales:
+// - Evita que me salte un error que detenga todo el Programa
+// - Convierte el Error en "undefined"
 const usuario1 = {
   nombre: "Ana",
   direccion: {
@@ -70,6 +107,36 @@ const usuario1 = {
 
 console.log(usuario1?.direccion?.ciudad); // Imprime: "Madrid"
 console.log(usuario1?.contacto?.telefono); // Imprime: undefined (no genera error)
+
+// - si existe ya no lo cambio
+// - si la izquierda en null o undefined le asignare esta
+// - rsm: le asigno el siguiente valor porque es nulo o undefined
+
+const a = { duration: 50 };
+
+a.speed ??= 25;
+console.log(a.speed);
+// Expected output: 25
+
+a.duration ??= 10;
+console.log(a.duration);
+// Expected output: 50
+
+// ┌───────────────────────────────────┐┌───────────────────────────────────┐
+// │                ??                 ││                 ?                 │
+// └───────────────────────────────────┘└───────────────────────────────────┘
+
+const foo = { someFooProp: "hola" };
+
+console.log(foo.someFooProp?.toUpperCase() ?? "no disponible"); // "hola"
+console.log(foo.someBarProp?.toUpperCase() ?? "no disponible"); // "no disponible"
+
+const customer = {
+  name: "Carl",
+  details: { age: 82 },
+};
+const customerCity = customer?.city ?? "Unknown city";
+console.log(customerCity); // Unknown city
 
 // ══════════════════════════════════════════════════════════════════════════
 //                         Operadores Especiales
